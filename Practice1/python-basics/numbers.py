314f58ae363ceace28962e791dce438fad5b05b5
Kalinur Atenov <kalinuratenov@MacBook-Air-Kalinur.local>
2026-01-27 15:43:00 +0500

    1

--- Practice1/python-basics/numbers.py
+++ Practice1/python-basics/numbers.py
@@ -1,3 +1,8 @@
+print(3)
+print(358)
+print(50000)
+
+
 x = 1    # int
 y = 2.8  # float
 z = 1j   # complex
@@ -14,18 +19,4 @@ print(type(x))
 print(type(y))
 print(type(z))
 
-x = 1.10
-y = 1.0
-z = -35.59
 
-print(type(x))
-print(type(y))
-print(type(z))
-
-x = 35e3
-y = 12E4
-z = -87.7e100
-
-print(type(x))
-print(type(y))
-print(type(z))
