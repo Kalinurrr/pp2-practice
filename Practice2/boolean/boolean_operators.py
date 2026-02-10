48384cb02b3524a6c2a10f818edd49089a49e5f7
Kalinur Atenov <kalinuratenov@MacBook-Air-Kalinur.local>
2026-02-10 16:21:47 +0500

    second practice

--- Practice2/boolean/boolean_operators.py
+++ Practice2/boolean/boolean_operators.py
@@ -2,7 +2,7 @@ x = 5
 print(x > 0 and x < 10)
 
 x = 5
-print(x < 5 or x > 10)
+print(x < 7 or x > 10)
 
 x = 5
 print(not(x > 3 and x < 10))
--- Practice2/if_else/if_elif_else.py
+++ Practice2/if_else/if_elif_else.py
@@ -8,8 +8,9 @@ else:
   print("a is greater than b")
 
 
-  a = 200
-b = 33
+a = 45
+b = 6
+
 if b > a:
   print("b is greater than a")
 else:
